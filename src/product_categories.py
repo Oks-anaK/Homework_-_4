81e3b6eb5d51b0b65e9688c9cdd82258efd9213e
Oksana Konkina <oksanka.808@mail.ru>
2025-07-03 15:15:46 +0300

    changed 3.07.25 .flake8

--- src/product_categories.py
+++ src/product_categories.py
@@ -29,4 +29,4 @@ class Category:
         self.products = products if products else []
 
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(self.products) if self.products else 0
